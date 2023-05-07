b76d6166291911fd86ba6a16588cc7a871638295
SYNOTEBOOK\liong <liongirl87@naver.com>
2023-05-08 00:40:38 +0900

    개인프로젝트 초기환경 세팅

---
+++ src/main/java/com/reviewcow/post/dao/PostMapper.java
@@ -0,0 +1,12 @@
+package com.reviewcow.post.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostMapper {
+
+	public List<Map<String, Object>> selectPostList();
+}
